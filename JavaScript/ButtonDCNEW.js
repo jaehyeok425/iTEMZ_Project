680895c47e72438abfffa41c23fae3fe72e3a133
jaehyeok425 <lincoln1604@gmail.com>
2024-06-14 13:30:24 +0900

    JS 수정
    JS 수정

--- JavaScript/ButtonDCNEW.js
+++ JavaScript/ButtonDCNEW.js
@@ -2,9 +2,9 @@ function redirectToURL(checkboxId) {
     var checkbox = document.getElementById(checkboxId);
     if (checkbox.checked) {
         if (checkboxId === 'checkbox1') {
-            window.location.href = "file:///C:/HomePage/DiscountProduct.html";
+            window.location.href = "DiscountProduct.html";
         } else if (checkboxId === 'checkbox2') {
-            window.location.href = "file:///C:/HomePage/NewProduct.html";
+            window.location.href = "NewProduct.html";
         }
     }
 }
--- JavaScript/PeriodofUse.js
+++ JavaScript/PeriodofUse.js
@@ -1,34 +1,34 @@
 // 첫 번째 버튼을 클릭할 때 실행되는 함수
 function navigateToPage1() {
-    window.location.href = 'file:///C:/HomePage/Permanent.html';
+    window.location.href = 'Permanent.html';
 }
 
 // 두 번째 버튼을 클릭할 때 실행되는 함수
 function navigateToPage2() {
-    window.location.href = 'file:///C:/HomePage/360.html';
+    window.location.href = '360.html';
 }
 
 // 세 번째 버튼을 클릭할 때 실행되는 함수
 function navigateToPage3() {
-    window.location.href = 'file:///C:/HomePage/150.html';
+    window.location.href = '150.html';
 }
 
 function navigateToPage4() {
-    window.location.href = 'file:///C:/HomePage/90.html';
+    window.location.href = '90.html';
 }
 
 function navigateToPage5() {
-    window.location.href = 'file:///C:/HomePage/60.html';
+    window.location.href = '60.html';
 }
 
 function navigateToPage6() {
-    window.location.href = 'file:///C:/HomePage/7.html';
+    window.location.href = '7.html';
 }
 
 function navigateToPage7() {
-    window.location.href = 'file:///C:/HomePage/3.html';
+    window.location.href = '3.html';
 }
 
 function navigateToPage8() {
-    window.location.href = 'file:///C:/HomePage/1.html';
+    window.location.href = '1.html';
 }
